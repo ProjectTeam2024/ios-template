46d816d9eb56fb1387c2255ab559ce1eba073804
SBJang <denlyou85@gmail.com>
2023-11-27 22:23:45 +0900

    Initial Commit

---
+++ ProjectTeam2024/ProjectTeam2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTeam2024App.swift
+//  ProjectTeam2024
+//
+//  Created by SB Jang on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTeam2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
